c4e9529d9e0d9b21e32728372f73a607416f2b8b
EliottLS <eliottlamseckjeudy@gmail.com>
2019-10-01 21:10:57 +0200

    Adding 19

---
+++ exo_19.rb
@@ -0,0 +1,9 @@
+Tableaumails = ["jean.dupont.01@email.fr"]
+for i in 2..50
+    Tableaumails << "jean.dupont.0#{i}@email.fr"
+    
+    if i%2 == 0
+    
+puts "jean.dupont.0#{i}@email.fr"
+    end 
+end 
